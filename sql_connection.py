2ed06ce8baef98348811efd69b170eff2e44c015
mrefugionv <mrefugionoriegav@gmail.com>
2026-04-06 16:30:48 -0600

    Working in sql_connection.py

--- sql_connection.py
+++ sql_connection.py
@@ -5,7 +5,7 @@ import mysql.connector
 config = {
     "host" :  "127.0.0.1",
     "port" : "3306",
-    "database" : "Hello mySQL",
+    "database" : "hello_mysql",
     "user" : "root",
     "password" : "Abuel1t4&YO!"
 }
@@ -13,12 +13,27 @@ config = {
 connection = mysql.connector.connect(**config)
 cursor = connection.cursor()
 
-query = "SELECT * FROM users;"
-cursor.execute(query)
-result = cursor.fetchall()
+def print_user(user):
+    query = "SELECT * FROM users WHERE name= %s;"
+    cursor.execute(query, (user,))
+    result = cursor.fetchall()
+    
+    for row in result :
+        print(row)
+    
+    cursor.close()
+    connection.close()
 
-for row in result :
-    print(row)
 
-cursor.close()
-connection.close()
+print_user("Brais")
+#print_user("DELETE TABLE users")
+
+'''
+SQL injection - cuidado con la concatenación de texto 
+cuando se quiere pasar parametro para la consulta por que 
+puede terminar siendo una instrucción peligrosa de perdida de información.
+Arriba la manera correcta de mandar parametros
+
+https://www.youtube.com/watch?v=OuJerKzV5T0
+6:00:00 
+'''
